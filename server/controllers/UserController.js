4b217f602198ec31c3fc7ff9e34570e721c0699b
MoneroLite <73478668+MoneroLite@users.noreply.github.com>
2022-11-01 18:22:45 +0500

    added server side authorization

---
+++ server/controllers/UserController.js
@@ -0,0 +1,115 @@
+import {validationResult} from 'express-validator'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken';
+
+import UserModel from '../models/User.js'
+
+export const register = async (req, res) => {
+    try {
+        const errors = validationResult(req)
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors.array())
+        }
+
+        const password = req.body.password
+        const salt = await bcrypt.genSalt(10);
+        const hash = await bcrypt.hash(password, salt)
+
+        const doc = UserModel({
+            email: req.body.email,
+            fullName: req.body.fullName,
+            avatarUrl: req.body.avatarUrl,
+            passwordHash: hash,
+        })
+        // сохранение данные в бд
+        const user = await doc.save()
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            }, 
+            process.env.SECRET_JWT_KEY,
+            {
+                expiresIn: '30d',
+            }
+        )
+
+        const {passwordHash, ...userData} = user._doc
+
+        res.json({
+            succes: true,
+            ...userData,
+            token,
+        })
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Не удалось зарегестрироваться'
+        })
+    }
+}
+
+export const login = async (req,res) => {
+    try {
+        const user = await UserModel.findOne({email: req.body.email})
+        if (!user) {
+            return res.status(400).json({
+                message: 'Неверный логин или пароль ^(',
+            })
+        }
+
+        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash)
+        if (!isValidPass) {
+            return req.status(400).json({
+                message: 'Неверный логин или пароль',
+            })
+        }
+
+        const token = jwt.sign(
+            {
+                _id: user._id,
+            }, 
+            process.env.SECRET_JWT_KEY,
+            {
+                expiresIn: '30d',
+            }
+        )
+
+        const {passwordHash, ...userData} = user._doc
+
+        res.json({
+            succes: true,
+            ...userData,
+            token,
+        })
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            message: 'Не удалось авторизоваться'
+        })
+    }
+}
+
+export const getMe = async (req, res) => {
+    try {
+        const user = await UserModel.findById(req.userId)
+        if (!user){
+            return res.status(404).json({
+                message: 'Пользователь не найден'
+            })
+        }
+        const {passwordHash, ...userData} = user._doc
+
+        res.json({
+            succes: true,
+            userData,
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Нет доступа'
+        })
+    }
+    
+}
--- server/index.js
+++ server/index.js
@@ -1,20 +1,24 @@
+import * as dotenv from 'dotenv'
+dotenv.config()
 import express from 'express';
+import mongoose from 'mongoose';
+import {registerValidation} from './validations/auth.js'
+import checkAuth from './utils/authCheck.js'
+import { getMe, login, register } from './controllers/UserController.js';
+
+mongoose
+.connect(process.env.DB_CONNECT)
+.then(() => console.log('connect to DB successfully'))
+.catch((err) => console.log('DB error', err))
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('HI!')
-})
-
-app.post('/auth/login', (req, res) => {
-    console.log(req.body)
-    res.json({
-        success: true,
-    })
-})
+app.post('/auth/login', login)
+app.post('/auth/register', registerValidation, register)
+app.get('/auth/me', checkAuth, getMe)
 
 app.listen(port, (err) => {
     if (err) {
